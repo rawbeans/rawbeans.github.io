e792d0d9ff4f655273f9dec157b70057e91e62fd
Robin Woodby <rlwoodby@gmail.com>
2016-08-17 17:16:51 -0700

    Hijacked for burn it down

---
+++ js/main-burn.js
@@ -0,0 +1,26 @@
+var target_date = new Date("January 1, 2020").getTime();
+var days, hours, minutes, seconds;
+var countdown = document.getElementById("countdown");
+
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value
+    countdown.innerHTML = days + "d, " + hours + "h, "
+    + minutes + "m, " + seconds + "s";  
+ 
+}, 1000);
